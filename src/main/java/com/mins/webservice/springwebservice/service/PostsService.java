7fca06992dfe223d4b89b42d5cb6e5599facdb02
amullo <mhyoonb@orga.co.kr>
2019-04-16 19:55:34 +0900

    board write form

---
+++ src/main/java/com/mins/webservice/springwebservice/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.mins.webservice.springwebservice.service;
+
+import com.mins.webservice.springwebservice.domain.posts.PostsRepository;
+import com.mins.webservice.springwebservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto dto){
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+}
--- src/main/java/com/mins/webservice/springwebservice/web/WebRestController.java
+++ src/main/java/com/mins/webservice/springwebservice/web/WebRestController.java
@@ -1,7 +1,7 @@
 package com.mins.webservice.springwebservice.web;
 
-import com.mins.webservice.springwebservice.domain.posts.PostsRepository;
 import com.mins.webservice.springwebservice.dto.posts.PostsSaveRequestDto;
+import com.mins.webservice.springwebservice.service.PostsService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello() {
@@ -21,6 +21,6 @@ public class WebRestController {
 
     @PostMapping("/posts")
     public void savePosts(@RequestBody PostsSaveRequestDto dto){
-        postsRepository.save(dto.toEntity());
+        postsService.save(dto);
     }
 }
