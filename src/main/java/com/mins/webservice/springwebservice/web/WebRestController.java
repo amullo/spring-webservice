13991c3a0ad40a1d1ee88a847d4e29c410188fae
amullo <mhyoonb@orga.co.kr>
2019-04-16 10:22:27 +0900

    posts error 수정

--- src/main/java/com/mins/webservice/springwebservice/web/WebRestController.java
+++ src/main/java/com/mins/webservice/springwebservice/web/WebRestController.java
@@ -2,12 +2,14 @@ package com.mins.webservice.springwebservice.web;
 
 import com.mins.webservice.springwebservice.domain.posts.PostsRepository;
 import com.mins.webservice.springwebservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
 
     private PostsRepository postsRepository;
