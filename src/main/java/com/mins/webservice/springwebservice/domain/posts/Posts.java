03e9cb5de1c373159db4689919c8507fc6270ad3
amullo <mhyoonb@orga.co.kr>
2019-04-16 19:07:27 +0900

    HardleBar apply Source

--- src/main/java/com/mins/webservice/springwebservice/domain/posts/Posts.java
+++ src/main/java/com/mins/webservice/springwebservice/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.mins.webservice.springwebservice.domain.posts;
 
+import com.mins.webservice.springwebservice.domain.BaseTimeEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity {
 
     @Id
     @GeneratedValue
